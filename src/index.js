3a230d8d95fddb097d85371e6e94a01f5b3a46a0
Leonardo Gatica <lgatica@protonmail.com>
2019-11-21 09:12:37 -0300

    refactor: update chilexpress api

--- src/index.js
+++ src/index.js
@@ -11,30 +11,30 @@ const ora = require('ora');
 
 updateNotifier({pkg}).notify();
 
-const getOrder = orderId => {
+const getOrder = command => {
+  const orderId = command.args[0];
   const spinner = ora('Searching...');
   spinner.start();
   chilexpress(orderId).then(data => {
     spinner.stop();
-    console.log(chalk.green(`Order ID: ${data.orderId}`));
-    console.log(chalk.green(`Transport ID: ${data.transportId}`));
-    console.log(chalk.green(`Product: ${data.product}`));
-    console.log(chalk.green(`Service: ${data.service}`));
+    console.log(chalk.green(`Order ID: ${orderId}`));
+    console.log(chalk.green(`Product: ${data.shipping.product}`));
+    console.log(chalk.green(`Service: ${data.shipping.service}`));
     console.log(chalk.green(`Status: ${data.status}`));
     const pattern = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):\d{2}\.\d+Z/;
-    if (data.isDeliveried) {
-      let deliveryHour = data.date.toISOString().replace(pattern, '$4') - 3;
+    if (data.status === 'Entregado') {
+      let deliveryHour = data.delivery.date.toISOString().replace(pattern, '$4') - 3;
       if (deliveryHour < 10) deliveryHour = `0${deliveryHour}`;
-      const deliveryDate = data.date.toISOString().replace(pattern, `$1-$2-$3 ${deliveryHour}:$5`);
+      const deliveryDate = data.delivery.date.toISOString().replace(pattern, `$1-$2-$3 ${deliveryHour}:$5`);
       console.log(chalk.green(`Delivery: ${deliveryDate}`));
-      console.log(chalk.green(`Receptor: ${data.receptor.name} (${data.receptor.rut})`));
+      console.log(chalk.green(`Receptor: ${data.delivery.name} (${data.delivery.rut})`));
     }
     console.log(chalk.green('History:'));
-    for (let i of data.history) {
-      let historyHour = i.date.toISOString().replace(pattern, '$4') - 3;
+    for (let history of data.history) {
+      let historyHour = history.date.toISOString().replace(pattern, '$4') - 3;
       if (historyHour < 10) historyHour = `0${historyHour}`;
-      let historyDate = i.date.toISOString().replace(pattern, `$1-$2-$3 ${historyHour}:$5`);
-      console.log(chalk.green(`${historyDate}: ${i.activity}`));
+      let historyDate = history.date.toISOString().replace(pattern, `$1-$2-$3 ${historyHour}:$5`);
+      console.log(chalk.green(`${historyDate}: ${history.activity}`));
     }
   }).catch(err => {
     spinner.stop();
